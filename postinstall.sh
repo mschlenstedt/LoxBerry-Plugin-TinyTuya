067c00753d049e715e9382f9401bb8a8e969008e
mschlenstedt <Michael@schlenn.net>
2023-06-17 18:10:34 +0200

    Use Original GIT Repo

--- postinstall.sh
+++ postinstall.sh
@@ -44,8 +44,7 @@ PBIN=$LBPBIN/$PDIR
 
 echo "<INFO> Installing TinyTuya"
 cd $PDATA
-#git clone https://github.com/jasonacox/tinytuya.git $PDATA
-git clone https://github.com/mschlenstedt/tinytuya.git -b mqtt_bugfix $PDATA
+git clone https://github.com/jasonacox/tinytuya.git $PDATA
 
 echo "<INFO> Creating Symlinks"
 rm $PDATA/server/mqtt/mqtt.json
